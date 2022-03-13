30eb7700c7b4f376ff1e09038bcdff9f86365fe1
Pravallika <96050882+prava-coder@users.noreply.github.com>
2022-03-13 11:42:40 +0530

    Add files via upload

---
+++ listtotuple.py
@@ -0,0 +1,7 @@
+#python program to convert a list to tuple
+#creating a list
+list1=["apple","banana","cherry","orange"]
+# to convert list into tuple
+listtotuple=tuple(list1)
+print(listtotuple)
+print(type(listtotuple))
